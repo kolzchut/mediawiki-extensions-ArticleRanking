8d26f41ff758f53cca3b294bc72000470ac95f56
Dror S. [ffs] <FreedomFighterSparrow@gmail.com>
2018-11-07 17:35:00 +0200

    When loading the Change Request form, add title+lang+categories
    This is the way the ShareBar extension used to work, and for some
    reason didn't migrate over here. The new implementation does it all
    in JavaScript, though.

--- modules/articleRanking.changeRequest.js
+++ modules/articleRanking.changeRequest.js
@@ -1,11 +1,21 @@
 ( function ( mw, $ ) {
 	'use strict';
-	var wgArticleRankingConfig = mw.config.get( 'wgArticleRankingConfig' );
+	var wgArticleRankingConfig = mw.config.get( 'wgArticleRankingConfig' ),
+		uri;
 
 	if ( wgArticleRankingConfig.changerequest !== null && wgArticleRankingConfig.changerequest.url ) {
+		uri = new mw.Uri( wgArticleRankingConfig.changerequest.url );
+		uri.extend( {
+			page: mw.config.get( 'wgTitle' ),
+			lang: mw.config.get( 'wgContentLanguage' ),
+			categories: mw.config.get( 'wgCategories' ).join()
+		} );
+
+		mw.log( uri.toString() );
+
 		$( '.ranking-btn.changerequest' ).click( function() {
 			mw.wrShareBar.openModal(
-				wgArticleRankingConfig.changerequest.url,
+				uri.toString(),
 				wgArticleRankingConfig.changerequest.width,
 				wgArticleRankingConfig.changerequest.height
 			);
