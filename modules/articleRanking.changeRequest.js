59da51e077fcb6fe060533c25c01ad47e22ae82b
Joel Rothschild <joel.rothschild@jacksonriver.com>
2023-08-14 12:28:36 +0300

    Refactored js to use the new KZChangeRequest extension's change request form.

--- modules/articleRanking.changeRequest.js
+++ modules/articleRanking.changeRequest.js
@@ -1,27 +1,41 @@
-( function () {
-	'use strict';
-	var config = mw.config.get( 'wgArticleRankingConfig' ),
-		uri,
-		selector;
-
-	if ( config.changerequest !== null && config.changerequest.url ) {
-		uri = new mw.Uri( config.changerequest.url );
-		uri.extend( {
-			page: mw.config.get( 'wgTitle' ),
-			lang: mw.config.get( 'wgContentLanguage' ),
-			categories: mw.config.get( 'wgCategories' ).join()
-		} );
-
-		selector = document.querySelector( '.ranking-btn.changerequest' );
-		if ( selector !== null ) {
-			selector.addEventListener( 'click', function () {
-				mw.wrShareBar.openModal(
-					uri.toString(),
-					config.changerequest.width,
-					config.changerequest.height,
-					{ title: mw.msg( 'ranking-cr-form-title' ) }
-				);
-			} );
-		}
-	}
-}() );
+$(function () {
+	mw.loader.using(['mediawiki.api'], function () {
+		$('.ranking-btn.changerequest').click(function (e) {
+			e.preventDefault();
+			// Modal markup
+			$('body').append(
+				'<div class="modal fade" id="changeRequestModal" tabindex="-1" role="dialog">'
+				+ '<div class="modal-dialog" role="document">'
+				+ '  <div class="modal-content">'
+				+ '    <div class="modal-header">'
+				+ '      <button type="button" class="close" id="changeRequestModalCloseButton" data-dismiss="modal" aria-label="Close">'
+				+ '        <span aria-hidden="true">&times;</span>'
+				+ '      </button>'
+				+ '    </div>'
+				+ '  <div class="modal-body" id="changeRequestModalBody">'
+				+ '    <div class="kzcr-spinner"><span class="kzcr-spin">&#10227;</span></div>'
+				+ '  </div>'
+				+ '</div>'
+				+ '<style>.kzcr-spinner{text-align:center;}.kzcr-spin{font-size:3em;position:relative;display:inline-block;-webkit-animation:spin 2s infinite linear;-moz-animation:spin 2s infinite linear;-o-animation:spin 2s infinite linear;animation:spin 2s infinite linear;}@-moz-keyframes spin{0%{-moz-transform:rotate(0deg);}100%{-moz-transform:rotate(359deg);}}@-webkit-keyframes spin{0%{-webkit-transform:rotate(0deg);}100%{-webkit-transform:rotate(359deg);}}@-o-keyframes spin{0%{-o-transform:rotate(0deg);}100%{-o-transform:rotate(359deg);}}@keyframes spin{0%{-webkit-transform:rotate(0deg);transform:rotate(0deg);}100%{-webkit-transform:rotate(359deg);transform:rotate(359deg);}}</style>'
+				+ '</div>'
+			);
+			// onClose handler
+			var onClose = function (e) {
+				$('#changeRequestModal').modal('hide');
+			};
+			// onReady handler
+			var onReady = function () {
+				$('#changeRequestModal').modal('handleUpdate');
+			};
+			// Open the modal.
+			$('#changeRequestModal').modal();
+			mw.loader.using('ext.KZChangeRequest.modal', function () {
+				window.kzcrAjax($('#changeRequestModalBody'), onClose, onReady);
+			});
+			// Dispose of the modal whenever it's closed.
+			$('#changeRequestModal').on('hidden.bs.modal', function (e) {
+				$('#changeRequestModal').remove();
+			});
+		});
+	});
+});
